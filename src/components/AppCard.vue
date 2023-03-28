7816d8497b50a98a8fd2addc6f19af85a470ddd2
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-28 17:14:07 +0200

    image issue

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,19 +1,24 @@
 <script>
     export default {
-        name: 'card'
+        name: 'card',
+        props: {
+            name: String,
+            image: String
+        }
     }
 </script>
 
 <template>
     <div class="card">
-        <img src="" alt="img card">
+        <img :src="image" alt="img card">
+        <div>nome: {{ name }}</div>
     </div>
     
 </template>
 
 <style lang="scss" scoped>
     .card {
-        width: 20%;
+        
         height: 140px;
 
         img {
--- src/components/AppListCards.vue
+++ src/components/AppListCards.vue
@@ -16,19 +16,27 @@
             axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=alien')
             .then((response)=> {
                 console.log(response);
-                this.dataCards = response.data;
+                this.dataCards = response.data.data;
+                console.log(this.dataCards)
             })
         }
     }
 </script>
 
 <template>
-    <div>
-        <div class="p-3 bg-black">Found X cards:</div>
-        <AppCard v-for="card in dataCards"/>
+    <div class="d-flex flex-wrap">
+        <div class="p-3 bg-black w-100">Found X cards:</div>
+        <div class="card-cont" v-for="(card, i) in dataCards">
+            <AppCard :name='card.name'
+                    :image="card.card_images.card.image_url"
+            />
+        </div>
+        
     </div>
 </template>
 
 <style lang="scss" scoped>
-   
+    .card-cont{
+        width: 20%;
+    }
 </style>
